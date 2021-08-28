2aa3f47bca5a2106b3e8c36375a0e8217a46dfd4
Bekzat Yernat <180107265@stu.sdu.edu.kz>
2021-08-28 20:36:53 +0600

    added movement

--- MovingPlatform.gd
+++ MovingPlatform.gd
@@ -4,14 +4,24 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var is_active = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func activate():
+	is_active = true
 	print("Platform started movement")
+	
+func move_platform():
+	if not is_active:
+		return
+	var velocity = move_and_slide(Vector2(0, -100))
+
+func _physics_process(delta):
+	move_platform()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
